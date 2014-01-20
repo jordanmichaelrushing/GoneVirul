7e7d164806ec3e03f7dc50571b6516b833a116a6
JordanRushing <Jordan@RushingFitness.com>
2014-01-20 17:16:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoneVirul::Application.config.secret_token = '5455e00ae3b79d79c1606b29bcac413f00abc027ddff129010f8409bd24e12d7ead1b46f61470c2cf9445d96c34dca381b2d7c5e6b09a5e8127c2d9d0f9607d5'
