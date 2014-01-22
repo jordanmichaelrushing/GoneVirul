57aecaf2021c047ceb35c4fb5326f4957a7675ea
JordanRushing <Jordan@RushingFitness.com>
2014-01-22 15:43:26 -0500

    Change path for cloud

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 Paperclip::Attachment.default_options[:url] = ":s3_alias_url"
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_alias] = 'http://d2ki179ibfama0.cloudfront.net/photos/'
+Paperclip::Attachment.default_options[:s3_host_alias] = 'd2ki179ibfama0.cloudfront.net/photos/'
 
 
