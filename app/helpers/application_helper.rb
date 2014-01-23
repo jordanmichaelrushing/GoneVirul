ddaf654b535044a25628f9be1a254a1a381ca38d
JordanRushing <Jordan@RushingFitness.com>
2014-01-23 18:57:31 -0500

    FIX TYPO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def smart_truncate(s, opts = {})
     if opts[:sentences]
       if opts[:description]
-        if a.split(".")[0].include? "<"
+        if s.split(".")[0].include? "<"
           if s.split(">")[2].split(".")[0].include? "<"
             return s.split(">")[1].split("<")[0] + s.split(">")[2].split("<")[0] + "..."
           else
