f00f34cbfa8a2ed2e130302e719b275e2719cb05
JordanRushing <Jordan@RushingFitness.com>
2014-01-23 11:44:11 -0500

    Make all landing pictures edit from the top down, instead of the midlle

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,11 @@
 class Photo < ActiveRecord::Base
 
   attr_accessible :blog_picture, :landing_picture
-  has_attached_file :landing_picture, :styles => { :landing => "590x200#", :side_bar => "300x157#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :landing_picture, :styles => { :landing => "", :side_bar => ""},
+    :convert_options => { 
+        :landing => "-gravity north -thumbnail 590x200^ -extent 590x200" ,
+        :side_bar => "-gravity north -thumbnail 300x157^ -extent 300x157"
+    }, :default_url => "/images/:style/missing.png"
   has_attached_file :blog_picture, :styles => { :blog => "600x447#"}, :default_url => "/images/:style/missing.png"
 
   belongs_to :blogs
