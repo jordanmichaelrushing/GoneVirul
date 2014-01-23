0e18bc19784260ecd1a2dd47f3c35382f6c1f5af
JordanRushing <Jordan@RushingFitness.com>
2014-01-23 18:53:28 -0500

    Fix small typo in meta description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,14 @@ module ApplicationHelper
   def smart_truncate(s, opts = {})
     if opts[:sentences]
       if opts[:description]
-        if s.include? "<"
+        if a.split(".")[0].include? "<"
           if s.split(">")[2].split(".")[0].include? "<"
             return s.split(">")[1].split("<")[0] + s.split(">")[2].split("<")[0] + "..."
           else
              return s.split(">")[2].split(".")[0] + "..."
            end
         else
-          return s.split(/\./).reject{ |s| s.strip.empty? }[0, opts[:sentences]].map{|s| s.strip}.join('. ')+"." + "..." 
+          return s.split(/\./).reject{ |s| s.strip.empty? }[0, opts[:sentences]].map{|s| s.strip}.join('. ') + "..." 
         end
       else
         return s.split(/\./).reject{ |s| s.strip.empty? }[0, opts[:sentences]].map{|s| s.strip}.join('. ')+".".html_safe 
